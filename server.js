0893387ca6a88902dd42d0cbf504f3bf6bcfce85
Amaan Baiyat <amaan_baiyat@live.com>
2018-12-28 20:46:33 -0600

    Added Middleware for Express

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const passportjwt = require('passport-jwt');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(
+  config.database,
+  { useNewUrlParser: true }
+);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to DB');
+});
+
+mongoose.connection.on('error', err => {
+  console.log('DB Error ' + err);
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// CORS Middleware
+app.use(cors());
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('', (req, res) => {
   res.send('Triage');
 });
 
-app.listen(port, () => console.log('Listening on port ' + port));
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
