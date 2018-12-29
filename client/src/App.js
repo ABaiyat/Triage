f8c2ca932bcc577736b36390b40ae610162c8a52
Amaan Baiyat <amaan_baiyat@live.com>
2018-12-29 00:47:12 -0600

    Added Skeleto Componentand adding Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Navigation from "./components/Navigation/Navigation";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Profile from "./components/Profile/Profile";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Triage</h1>
+        <BrowserRouter>
+          <div>
+            <Navigation />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/profile" component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
