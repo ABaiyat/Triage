5eb80cdb218ffabf7e5997742f22294179980fcb
Amaan Baiyat <amaan_baiyat@live.com>
2018-12-30 19:15:22 -0600

    Added Register and Login Layouts

--- client/src/components/Login/LoginComponent.jsx
+++ client/src/components/Login/LoginComponent.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Button, Input, Form, Card } from "semantic-ui-react";
 
 const LoginComponent = ({ simpleAction, simpleReducer }) => {
   return (
     <div>
-      <h1>Login</h1>
+      <Card centered>
+        <Card.Content>
+          <h1>Login</h1>
+          <Form>
+            <Form.Field>
+              <Input icon="users" iconPosition="left" placeholder="Username" />
+            </Form.Field>
+            <Form.Field>
+              <Input icon="key" iconPosition="left" placeholder="Password" />
+            </Form.Field>
+            <Form.Field>
+              <Button fluid color="blue">
+                Login
+              </Button>
+            </Form.Field>
+          </Form>
+        </Card.Content>
+      </Card>
+      <Card centered>
+        <Card.Content>
+          <p className="registerText">
+            Need an Account? <a href="/register">Register</a>
+          </p>
+        </Card.Content>
+      </Card>
     </div>
   );
 };
--- client/src/components/Register/RegisterComponent.jsx
+++ client/src/components/Register/RegisterComponent.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Button, Input, Form, Card, Grid } from "semantic-ui-react";
 
 const RegisterComponent = () => {
-  return <h1>Register</h1>;
+  return (
+    <div>
+      <Card centered>
+        <Card.Content>
+          <h1>Register</h1>
+          <Form>
+            <Form.Field>
+              <Input
+                name="name"
+                icon="user outline"
+                iconPosition="left"
+                placeholder="Name"
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                name="email"
+                icon="at"
+                iconPosition="left"
+                placeholder="Email"
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                name="username"
+                icon="users"
+                iconPosition="left"
+                placeholder="Username"
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                name="password"
+                type="password"
+                icon="key"
+                iconPosition="left"
+                placeholder="Password"
+              />
+            </Form.Field>
+            <Form.Field>
+              <Button fluid color="blue">
+                Register
+              </Button>
+            </Form.Field>
+          </Form>
+        </Card.Content>
+      </Card>
+      <Card centered>
+        <Card.Content>
+          <p className="registerText">
+            Already Registered? <a href="/login">Login</a>
+          </p>
+        </Card.Content>
+      </Card>
+    </div>
+  );
 };
 
 export default RegisterComponent;
